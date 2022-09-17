8fcd3041e27e8cf87a42dfaec6e54f0aa28dbef6
Victor Wang <victorwang2001@gmail.com>
2022-09-17 17:56:17 -0400

    clean up code

--- src/test_circuit.py
+++ src/test_circuit.py
@@ -20,6 +20,7 @@ print(measurements.get_counts())
 
 class PauliSandwichBackend(QiskitSimulator):
     def __init__(self, U, bread_gates, inner_backend):
+        print("init", U, bread_gates, inner_backend)
         self.U = U
         self.bread_gates = bread_gates
         self.inner_backend = inner_backend
@@ -28,18 +29,21 @@ class PauliSandwichBackend(QiskitSimulator):
         data_qubit_indices = tuple(range(circuit.n_qubits))
         new_circuit = Circuit([])
         n_sandwiches = 0
+
         for operation in circuit.operations:
             if operation.gate is self.U:
                 for P in self.bread_gates:
                     n_swandwiches +=1
                     op_indices = operation.qubit_indices
                     control_qubit_index = circuit.num_qubits + n_sandwiches
-                    controlled_P_qubits = (control_qubit_index,) = data_qubit_indices
-                    Pprime = U(*op_indices) * P * U.gate.dagger(*op_indices) # make this run faster
-                    new_circuit += Pprime.gate.controlled(1)(*controlled_p_qubits)
+                    controlled_P_qubits = (control_qubit_index,) + data_qubit_indices
+                    Pprime = self.U(*op_indices) * P * self.U.gate.dagger(*op_indices) # make this run faster
+                    new_circuit += Pprime.gate.controlled(1)(*controlled_P_qubits)
             else:
                 new_circuit += operation
-        raw_meas = self.innder_backend.run_circuit_and_measure(new_circuit, n_samples)
+
+        raw_meas = self.inner_backend.run_circuit_and_measure(new_circuit, n_samples)
+
         raw_counts  = raw_meas.get_counts()
         sandwiched_counts = {}
         for key in raw_counts.keys():
@@ -47,13 +51,11 @@ class PauliSandwichBackend(QiskitSimulator):
                 sandwiched_counts[key[:circuit.n_qubits]] = raw_counts[key]
         return Measurements.from_counts(sandwiched_counts)
 
-"""
-from orquestra.quantum.backends import PauliSandwichBackend
+# from orquestra.quantum.backends import PauliSandwichBackend
 
 sandwiched_qiskit_backend = PauliSandwichBackend(CNOT, None, qiskit_sim)
-measurements = qiskit_sim.run_circuit_and_measure(circ, 1000)
+measurements = sandwiched_qiskit_backend.run_circuit_and_measure(circ, 1000)
 
 # after sandwiching, we should have no errors
 # getting the dictionary {"111", 1000} indicates errors have been eliminated
 print(measurements.get_counts())
-"""
